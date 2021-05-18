a099fbad35343ec11c32b95d3e107b409abef502
itshmnt <hemantxs5@gmail.com>
2021-05-18 20:08:50 +0530

    Extracting Route Parameters

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,6 +5,7 @@ import { Route, NavLink } from 'react-router-dom';
 import './Blog.css';
 import Posts from '../Blog/Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render () {
@@ -32,7 +33,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" component={NewPost} />
-                {/* <Route path="/" exact component={Posts} /> */}
+                <Route path="/:id" exact component={FullPost} />
             </div>
         );
     }
